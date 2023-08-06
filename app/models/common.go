b4cc3982d3b45324c6e615c27aacb6a346c6b153
罗天宇 <2512448214@qq.com>
2023-08-06 15:29:56 +0800

    fix: 修改全局配置

--- app/models/common.go
+++ app/models/common.go
@@ -4,16 +4,16 @@ import "time"
 
 // ID 自增主键
 type ID struct {
-	ID uint `json:"id" gorm:"primaryKey"`
+	ID uint `json:"id" gorm:"primaryKey" column:"id"`
 }
 
 // Timestamp 创建与更新时间
 type Timestamp struct {
-	CreateTime time.Time `json:"create_time"`
-	UpdateTime time.Time `json:"update_time"`
+	CreateTime time.Time `json:"create_time" column:"create_time"`
+	UpdateTime time.Time `json:"update_time" column:"update_time"`
 }
 
 // PseudoDeletion 伪删除
 type PseudoDeletion struct {
-	DeleteTime time.Time `json:"delete_time"`
+	DeleteTime time.Time `json:"delete_time" column:"delete_time"`
 }
