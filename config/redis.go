80076f70fe9c0d4af75c8a956ff460bf3b60df04
dabanguai <1596815312@qq.com>
2023-08-14 08:14:34 +0800

    回滚

--- config/redis.go
+++
@@ -1,8 +0,0 @@
-package config
-
-type Redis struct {
-	Host     string `mapstructure:"172.16.32.69" json:"host" yaml:"host"`
-	Port     int    `mapstructure:"50531" json:"port" yaml:"port"`
-	DB       int    `mapstructure:"1" json:"db" yaml:"db"`
-	Password string `mapstructure:"XFAWVFpF" json:"password" yaml:"password"`
-}
