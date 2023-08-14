1452833eab1d51c6b56fdad5608aa007f092b565
dabanguai <1596815312@qq.com>
2023-08-14 09:04:36 +0800

    Revert "Revert "发现配置信息好像是通过其他文件配置好的""
    This reverts commit 1186877b

--- config/jwt.go
+++ config/jwt.go
@@ -2,8 +2,8 @@ package config
 
 // Jwt token结构体
 type Jwt struct {
-	Secret                  string `mapstructure:"NoduBipzAWkVQzGGIlWGXf8p0QQfWZG1YG6sbF5aLg4z8N9BrF" json:"secret" yaml:"secret"`
-	JwtTtl                  int64  `mapstructure:"43200" json:"jwt_ttl" yaml:"jwt_ttl"`                                    // token 有效期（秒）
-	JwtBlacklistGracePeriod int64  `mapstructure:"10" json:"jwt_blacklist_grace_period" yaml:"jwt_blacklist_grace_period"` // 黑名单宽限时间（秒）
-	RefreshGracePeriod      int64  `mapstructure:"1800" json:"refresh_grace_period" yaml:"refresh_grace_period"`           // token 自动刷新宽限时间（秒）
+	Secret                  string `mapstructure:"secret" json:"secret" yaml:"secret"`
+	JwtTtl                  int64  `mapstructure:"jwt_ttl" json:"jwt_ttl" yaml:"jwt_ttl"`                                                          // token 有效期（秒）
+	JwtBlacklistGracePeriod int64  `mapstructure:"jwt_blacklist_grace_period" json:"jwt_blacklist_grace_period" yaml:"jwt_blacklist_grace_period"` // 黑名单宽限时间（秒）
+	RefreshGracePeriod      int64  `mapstructure:"refresh_grace_period" json:"refresh_grace_period" yaml:"refresh_grace_period"`                   // token 自动刷新宽限时间（秒）
 }
