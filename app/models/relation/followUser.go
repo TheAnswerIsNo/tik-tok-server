408cb5d73c3ffab086a1b508601a9d05baa2c7a4
dabanguai <1596815312@qq.com>
2023-08-16 02:17:30 +0800

    更新关注和取消关注操作

--- app/models/relation/followUser.go
+++ app/models/relation/followUser.go
@@ -1,54 +1,100 @@
 package relation
 
-//
-//import (
-//	"log"
-//	"sync"
-//	"tik-tok-server/bootstrap"
-//)
-//
-//// 创建user结构体
-//type UserInfo struct {
-//	Id             int32       `json:"id,omitempty" :"id"`
-//	Name           string      `json:"name,omitempty" :"name"`
-//	sex            bool        `json:"sex,omitempty" :"sex"`
-//	getLike        int64       `json:"get_like,omitempty" :"get_like"`
-//	Friends        int64       `json:"friends,omitempty" :"friends"`
-//	Follows        []*UserInfo `json:"follow,omitempty" :"follows"`
-//	Followed       []*UserInfo `json:"followed,omitempty" :"followed"`
-//	Videos         []*Video    `:"videos"`
-//	FavoriteVideos []*Video    `:"favorite_videos"`
-//	Comments       []*Comment  `:"comments"`
-//}
-//
-//type UserDao struct {
-//}
-//
-//var (
-//	userOnce sync.Once
-//	userDao  *UserDao
-//)
-//
-//func NewUserDao() *UserDao {
-//	userOnce.Do(func() {
-//		userDao = new(UserDao)
-//	})
-//	return userDao
-//}
-//
-//// 通过Id查找用户是否存在
-//func (user *UserDao) IsExistById(id int64) bool {
-//	var userInfo UserInfo
-//	db := bootstrap.InitalizeDB()
-//	if err := db.Where("id = ?", id).Select("id").First(&userInfo).Error; err != nil {
-//		log.Println(err)
-//	}
-//	if userInfo.Id == 0 {
-//		return false
-//	}
-//	return true
-//}
-//
-//func (user *UserDao) AddUserFollow() {
-//
-//}
+import (
+	"gorm.io/gorm"
+	"log"
+	"sync"
+	"tik-tok-server/app/models/comment"
+	"tik-tok-server/app/models/video"
+	"tik-tok-server/bootstrap"
+)
+
+// 创建user结构体
+type UserInfo struct {
+	UserId         int32              `json:"user_id,omitempty"`
+	Name           string             `json:"name,omitempty"`
+	sex            bool               `json:"sex,omitempty"`
+	getLike        int64              `json:"get_like,omitempty"`
+	Friends        int64              `json:"friends,omitempty"`
+	FollowCount    int64              `json:"follow_count"`   // 关注数
+	FollowerCount  int64              `json:"follower_count"` // 粉丝数
+	UserRelation   []*UserInfo        `json:"user_relation"`
+	Followed       []*UserInfo        `json:"followed,omitempty"`
+	Videos         []*video.Video     `json:"videos,omitempty"`
+	FavoriteVideos []*video.Video     `json:"favorite_videos,omitempty"`
+	Comments       []*comment.Comment `json:"comments,omitempty"`
+}
+
+// 生成对应表
+func (UserInfo) TableName() string {
+	return "user_info"
+}
+
+// Dao结构体 用于封装数据库操作
+type UserDao struct {
+}
+
+var (
+	userOnce sync.Once
+	userDao  *UserDao
+)
+
+// 实例化函数
+func NewUserDao() *UserDao {
+	userOnce.Do(func() {
+		userDao = new(UserDao)
+	})
+	return userDao
+}
+
+// 通过Id查找用户是否存在
+func (*UserDao) IsExistById(id int64) bool {
+	var userInfo UserInfo
+	db := bootstrap.InitalizeDB()
+	if err := db.Where("user_id = ?", id).Select("id").First(&userInfo).Error; err != nil {
+		log.Println(err)
+	}
+	if userInfo.UserId == 0 {
+		return false
+	}
+	return true
+}
+
+// 用户关注
+// userId: 当前用户id，followedId:被关注的对象的id
+func (user *UserDao) AddUserFollow(userId, followedId int64) error {
+	return bootstrap.Db.Transaction(func(tx *gorm.DB) error {
+		// 更新当前用户的关注数
+		if err := tx.Exec("UPDATE user_info SET follow_count = follow_count + 1 WHERE id = ?", userId).Error; err != nil {
+			return err
+		}
+		// 更新被关注用户的粉丝数
+		if err := tx.Exec("UPDATE user_info SET follower_count = follower_count + 1 WHERE id = ?", followedId).Error; err != nil {
+			return err
+		}
+		//	更新用户关系
+		if err := tx.Exec("INSERT INTO `user_relation` (`user_info_id`, `follow_id`) VALUES (?, ?)", userId, followedId).Error; err != nil {
+			return err
+		}
+		return nil
+	})
+}
+
+// 取消关注
+func (user *UserDao) CancelUserFollow(userId, followedId int64) error {
+	return bootstrap.Db.Transaction(func(tx *gorm.DB) error {
+		// 更新当前用户的关注数
+		if err := tx.Exec("UPDATE user_info SET follow_count = follow_count - 1 WHERE id = ?", userId).Error; err != nil {
+			return err
+		}
+		// 更新被关注用户的粉丝数
+		if err := tx.Exec("UPDATE user_info SET follower_count = follower_count - 1 WHERE id = ?", followedId).Error; err != nil {
+			return err
+		}
+		//	更新用户关系
+		if err := tx.Exec("DELETE FROM `user_relation` (`user_info_id`, `follow_id`) VALUES (?, ?)", userId, followedId).Error; err != nil {
+			return err
+		}
+		return nil
+	})
+}
