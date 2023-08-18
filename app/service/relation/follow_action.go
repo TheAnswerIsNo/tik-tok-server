f16142ccb14431b0b942598fd226a9dde4ac7c50
dabanguai <1596815312@qq.com>
2023-08-18 10:01:59 +0800

    完善关注业务

--- app/service/relation/follow_action.go
+++ app/service/relation/follow_action.go
@@ -1,6 +1,9 @@
 package relation
 
-import "tik-tok-server/app/models/relation"
+import (
+	"errors"
+	"tik-tok-server/app/models/relation"
+)
 
 const (
 	FOLLOW = 1
@@ -8,7 +11,10 @@ const (
 )
 
 // 自定义错误
-var ()
+var (
+	ErrInvalidAct  = errors.New("未定义操作")
+	ErrInavlidUser = errors.New("关注用户不存在")
+)
 
 type FollowActionFlow struct {
 	userId     int64
@@ -30,7 +36,7 @@ func (fc *FollowActionFlow) Do() error {
 	if err = fc.check(); err != nil {
 		return err
 	}
-	if err = fc.publish(); err != nil {
+	if err = fc.packData(); err != nil {
 		return err
 	}
 	return nil
@@ -40,7 +46,26 @@ func (fc *FollowActionFlow) Do() error {
 func (fc *FollowActionFlow) check() error {
 	//	关注用户已注销
 	if !relation.NewUserDao().IsExistById(fc.followedId) {
-		return error
+		// 待解决，如何自定义错误
+		return ErrInavlidUser
 	}
+	if fc.actionType != FOLLOW && fc.actionType != CANCEL {
+		return ErrInvalidAct
+	}
+	// 自己关注自己是否需要
 	return nil
+}
+
+func (fc *FollowActionFlow) packData() error {
+	userDao := relation.NewUserDao()
+	var err error
+	switch fc.actionType {
+	case FOLLOW:
+		err = userDao.AddUserFollow(fc.userId, fc.followedId)
+	case CANCEL:
+		err = userDao.CancelUserFollow(fc.userId, fc.followedId)
+	default:
+		return ErrInvalidAct
+	}
+	return err
 }
